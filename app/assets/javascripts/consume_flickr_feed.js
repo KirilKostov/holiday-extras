bda3b58d5740936a488b393a8f24117c7adf64e8
Kiril Chitinov <k.chitinov@gmail.com>
2017-12-15 00:41:51 +0200

    Refactor handling of 'description' and 'tags' data pieces

--- app/assets/javascripts/consume_flickr_feed.js
+++ app/assets/javascripts/consume_flickr_feed.js
@@ -2,12 +2,17 @@ function createFlickrBox(item) {
   var template = $('.flickr-box-template').clone(),
       authorUrl = 'https://www.flickr.com/photos/' + item.author_id,
       authorName = item.author.match(/\("(.*)"\)/)[1],
-      tags = item.tags.trim().split(' ').join(', ');
+      rawDescription = document.createElement('div'),
+      description,
+      tags = item.tags ? item.tags.trim().split(' ').join(', ') : 'N/A';
+
+      rawDescription.innerHTML = item.description;
+      description = rawDescription.querySelectorAll('p')[2] ? rawDescription.querySelectorAll('p')[2].innerHTML : 'N/A';
 
   template.find('img').attr('src', item.media.m);
   template.find('.photo-title').attr('href', item.link).text(item.title);
   template.find('.author').attr('href', authorUrl).text(authorName);
-  template.find('.description').html(item.description.trim());
+  template.find('.description span').html(description);
   template.find('.tags span').text(tags);
   template.removeClass('flickr-box-template').addClass('flickr-box');
 
